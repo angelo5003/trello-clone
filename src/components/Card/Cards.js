cd2fadefd5c65b9f59d19149ec1fa2569ab5b983
Tony5300 <tony_tolen@ziggo.nl>
2021-12-15 17:22:01 +0100

    removed stuff that is not good.

--- src/components/Card/Cards.js
+++ src/components/Card/Cards.js
@@ -4,12 +4,6 @@ import "./Cards.css";
 const Cards = () => {
   const [title, setTitle] = useState("");
   const [isEditing, setEditing] = useState(false);
-  const [counter, setCounter] = useState(0);
-
-  const handleCount = () => {
-    setCounter(counter + 1);
-    console.log(counter);
-  };
 
   const handleTitle = (event) => {
     event.preventDefault();
@@ -23,17 +17,32 @@ const Cards = () => {
           {isEditing ? null : (
             <h3 onChange={() => setEditing(!isEditing)}>{title}</h3>
           )}
+
           <input
-            className="card-title"
             type="text"
             onChange={handleTitle}
             value={title}
             placeholder="Enter Title"
           />
-          {Array.from(Array(counter)).map((number, index) => {
-            return <input key={number} type="text" />;
-          })}
-          <button onClick={handleCount}>Add a card</button>
+
+          <button>Add Task</button>
+        </div>
+      </div>
+
+      {/* <div className="card-content">
+        <div className="card-info">
+          {isEditing ? null : (
+            <h3 onChange={() => setEditing(!isEditing)}>{title}</h3>
+          )}
+          <input
+            type="text"
+            onChange={handleTitle}
+            value={title}
+            placeholder="Enter Title"
+          />
+          <p>Some text</p>
+          <p>Some text</p>
+          <button>Add Task</button>
         </div>
       </div>
 
@@ -43,7 +52,6 @@ const Cards = () => {
             <h3 onChange={() => setEditing(!isEditing)}>{title}</h3>
           )}
           <input
-            className="card-title"
             type="text"
             onChange={handleTitle}
             value={title}
@@ -51,27 +59,9 @@ const Cards = () => {
           />
           <p>Some text</p>
           <p>Some text</p>
-          <button>Add a card</button>
+          <button>Add Task</button>
         </div>
-      </div>
-
-      <div className="card-content">
-        <div className="card-info">
-          {isEditing ? null : (
-            <h3 onChange={() => setEditing(!isEditing)}>{title}</h3>
-          )}
-          <input
-            className="card-title"
-            type="text"
-            onChange={handleTitle}
-            value={title}
-            placeholder="Enter Title"
-          />
-          <p>Some text</p>
-          <p>Some text</p>
-          <button>Add a card</button>
-        </div>
-      </div>
+      </div> */}
     </div>
   );
 };
